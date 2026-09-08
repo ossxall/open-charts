d24c0e12bb5eba8bdf89cc7d89a551ff1fdcb458
Juan Rey <juanjo9841@gmail.com>
2026-09-07 22:37:01 -0500

    Update Candlestick.ts

--- playground/series/Candlestick/Candlestick.ts
+++ playground/series/Candlestick/Candlestick.ts
@@ -141,7 +141,7 @@ export const Candlestick = (config: CandlestickConfig) => {
     layer: config.layer,
     priceTagColor: config.priceTagColor,
     width: config.width ?? "100%",
-    height: config.height ?? "calc(100vh - (2.5rem * 3) - (0.25rem * 2))",
+    height: config.height ?? "calc(100vh - (2.5rem * 3) - (0.25rem * 3))",
     params: config.params,
 
     compute(data: CandleBubble[]): any[] {
